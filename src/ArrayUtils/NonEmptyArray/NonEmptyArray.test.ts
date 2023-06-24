f5cfc9160be95555e50857ba2be330f907c27483
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2023-06-24 14:54:28 -0500

    fix: :white_check_mark: fix NonEmptyArray test

--- src/ArrayUtils/NonEmptyArray/NonEmptyArray.test.ts
+++ src/ArrayUtils/NonEmptyArray/NonEmptyArray.test.ts
@@ -4,13 +4,13 @@ describe("[Test] NonEmptyArray", () => {
   it("returns the first element of a non-empty array", () => {
 
 
-    function esNonEmptyArray<T>(arr: T[]): arr is NonEmptyArray<T> {
-      return arr.length > 0;
+    function esNonEmptyArray<T>(arr: NonEmptyArray<T>) {
+      return arr[0];
     }
 
     const arr = [1, 2, 3, 4, 5];
     if (arr.length > 0) {
-      const primer = esNonEmptyArray<number>(arr);
+      const primer = esNonEmptyArray<number>([arr[0]]);
       expect(primer).toBe(1);
     }
   });
