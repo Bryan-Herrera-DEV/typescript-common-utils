509e45699524630d1b1bf5ef36299196e487c8b8
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2023-07-04 10:04:36 -0500

    feat: :zap: add various changes

--- src/ClassUtils/PublicMethods/PublicMethods.test.ts
+++ src/ClassUtils/PublicMethods/PublicMethods.test.ts
@@ -2,8 +2,8 @@ import { PublicMethods } from ".";
 import { expectType } from "tsd";
 
 class MiClase {
-  public nombre: string = 'Juan';
-  public apellido: string = 'Perez';
+  public nombre: string = "Juan";
+  public apellido: string = "Perez";
 
   public saludar() {
     return `Hola, mi nombre es ${this.nombre} ${this.apellido}`;
@@ -14,7 +14,7 @@ describe("[Test] PublicMethods", () => {
   it("returns the public methods of a class", () => {
     const obtenerMetodosPublicos = <T extends { [key: string]: any }>(obj: T): Array<PublicMethods<T>> => {
       const keys = Object.getOwnPropertyNames(Object.getPrototypeOf(obj)).filter(
-        key => typeof obj[key] === 'function' && key !== 'constructor'
+        key => typeof obj[key] === "function" && key !== "constructor"
       ) as Array<PublicMethods<T>>;
       return keys;
     };
@@ -24,6 +24,6 @@ describe("[Test] PublicMethods", () => {
     const resultado = obtenerMetodosPublicos(miObjeto);
 
     expectType<Array<PublicMethods<MiClase>>>(resultado);
-    expect(resultado).toEqual(['saludar']);
+    expect(resultado).toEqual(["saludar"]);
   });
 });
